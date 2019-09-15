413e69c94da8a7f829471313bb6bb621264e95f4
SnapDragon <maliktahir0702@gmail.com>
2019-09-15 19:13:55 +0200

    Uniborg files support test#1

---
+++ uniborg/util.py
@@ -0,0 +1,20 @@
+from cyborg.util import admin_cmd as ac
+from cyborg.util import is_read as ir
+from cyborg.util import progress as pg
+from cyborg.util import humanbytes as hb
+from cyborg.util import time_formatter as tf
+
+def admin_cmd(pattern=None, **args):
+    ac(pattern, args)
+
+async def is_read(borg, entity, message, is_out=None):
+    ir(borg, entity, message, is_out)
+
+async def progress(current, total, event, start, type_of_ps):
+    pg(current, total, event, start, type_of_ps)
+
+def humanbytes(size):
+    hb(size)
+
+def time_formatter(milliseconds: int) -> str:
+    tf(milliseconds)
