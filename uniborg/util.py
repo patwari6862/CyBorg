90dfd7ed9edd5a654b9ab84a03cc4f59fc3a90c9
SnapDragon <maliktahir0702@gmail.com>
2019-09-15 20:15:58 +0200

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -1,20 +1,21 @@
-from cyborg.util import admin_cmd as ac
+from telethon import events
 from cyborg.util import is_read as ir
 from cyborg.util import progress as pg
 from cyborg.util import humanbytes as hb
 from cyborg.util import time_formatter as tf
 
-def admin_cmd(pattern=None, allow_sudo=True, outgoing=True, incoming=False, blacklist_chats=True, allow_edited_updates=False):
-    ac(pattern)
+def admin_cmd(pattern=None, allow_sudo=True, outgoing=True, incoming=False, allow_edited_updates=False):
+    pattern = Config.COMMAND_HAND_LER + pattern
+    events.NewMessage(pattern, allow_sudo, outgoing, incoming, allow_edited_updates)
 
 async def is_read(borg, entity, message, is_out=None):
     ir(borg, entity, message, is_out)
 
-async def progress(current, total, event, start, type_of_ps):
-    pg(current, total, event, start, type_of_ps)
+def progress(current, total, event, start, type_of_ps):
+    return pg(current, total, event, start, type_of_ps)
 
 def humanbytes(size):
-    hb(size)
+    return hb(size)
 
 def time_formatter(milliseconds: int) -> str:
-    tf(milliseconds)
+    return tf(milliseconds)
