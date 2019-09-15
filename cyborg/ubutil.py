6f483ce9a673953a3c1b0659657f4d58d4f03a1e
SnapDragon <maliktahir0702@gmail.com>
2019-09-15 21:27:46 +0200

    Update ubutil.py

--- cyborg/ubutil.py
+++ cyborg/ubutil.py
@@ -19,39 +19,36 @@ else:
         from config import Development as Config
 
 
-def admin_cmd(pattern=None, **args):
-    args["allow_sudo"] = True
-    allow_sudo = args["allow_sudo"]
+def admin_cmd(pattern=None, allow_sudo=True, outgoing=True, incoming=False, allow_edited_updates=False):
 
     # get the pattern from the decorator
     if pattern is not None:
         if pattern.startswith("\#"):
             # special fix for snip.py
-            args["pattern"] = re.compile(pattern)
+            pattern = re.compile(pattern)
         else:
-            args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
+            pattern = re.compile(Config.COMMAND_HAND_LER + pattern)
 
-    args["outgoing"] = True
     # should this command be available for other users?
     if allow_sudo:
-        args["from_users"] = list(Config.SUDO_USERS)
+        from_users = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
-        args["incoming"] = True
-        del args["allow_sudo"]
+        incoming = True
+        del allow_sudo
 
     # error handling condition check
-    elif "incoming" in args and not args["incoming"]:
-        args["outgoing"] = True
+    elif bool(incoming):
+        outgoing = True
 
     # add blacklist chats, UB should not respond in these chats
-    args["blacklist_chats"] = True
+    blacklist_chats = True
     black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
     if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
+        chats = black_list_chats
 
     # check if the plugin should allow edited updates
     allow_edited_updates = False
-    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+    if bool(allow_edited_updates):
         allow_edited_updates = args["allow_edited_updates"]
         del args["allow_edited_updates"]
 
