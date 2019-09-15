515dd4478ab1a56445117fa2659badab609aa285
SnapDragon <maliktahir0702@gmail.com>
2019-09-15 21:21:12 +0200

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -1,10 +1,10 @@
 from telethon import events
 from sample_config import Config
-from cyborg.util import admin_cmd as ac
-from cyborg.util import is_read as ir
-from cyborg.util import progress as pg
-from cyborg.util import humanbytes as hb
-from cyborg.util import time_formatter as tf
+from cyborg.ubutil import admin_cmd as ac
+from cyborg.ubutil import is_read as ir
+from cyborg.ubutil import progress as pg
+from cyborg.ubutil import humanbytes as hb
+from cyborg.ubutil import time_formatter as tf
 
 def admin_cmd(pattern=None, allow_sudo=True, outgoing=True, incoming=False, allow_edited_updates=False):
     return ac(pattern)
