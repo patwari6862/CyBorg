649b8897abe7f64ce4536eba9359366874ef3c39
SnapDragon <maliktahir0702@gmail.com>
2019-09-16 21:53:47 +0200

    Update __init__.py

--- cyborg/__init__.py
+++ cyborg/__init__.py
@@ -8,13 +8,3 @@ import sys
 sys.modules['uniborg.util'] = compat.uniborg.util
 # ...
 from uniborg.util import admin_cmd, is_read, progress, humanbytes, time_formatter
-print(admin_cmd.__file__)
-print(dir(admin_cmd))
-print(is_read.__file__)
-print(dir(is_read))
-print(progress.__file__)
-print(dir(progress))
-print(humanbytes.__file__)
-print(dir(humanbytes))
-print(time_formatter.__file__)
-print(dir(time_formatter))
