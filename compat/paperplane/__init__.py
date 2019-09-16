62c575b6c46ca21735be3aa886285a1734c172e9
SnapDragon <maliktahir0702@gmail.com>
2019-09-17 00:41:36 +0200

    Update __init__.py

--- compat/paperplane/__init__.py
+++ compat/paperplane/__init__.py
@@ -8,6 +8,7 @@
 import os
 
 from sys import version_info
+from stdplugins.unite import borgs
 from telethon import events
 from logging import basicConfig, getLogger, INFO, DEBUG
 from distutils.util import strtobool as sb
@@ -127,7 +128,7 @@ TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY",
                                          "./downloads")
 
 # 'bot' variable
-bot = borg
+bot = borgs
 
 
 # Global Variables
