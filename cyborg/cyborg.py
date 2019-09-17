6b2440012164439eb716ce9f22a07dc6fb95a6a2
SnapDragon <maliktahir0702@gmail.com>
2019-09-17 19:06:31 +0200

    Update cyborg.py

--- cyborg/cyborg.py
+++ cyborg/cyborg.py
@@ -59,16 +59,16 @@ class Cyborg(TelegramClient):
 
         inline_bot_plugin = Path(__file__).parent / "_inline_bot.py"
         self.load_plugin_from_file(inline_bot_plugin)
-        
-        for a_plugin_path in Path().glob(f"compat/paperplane/*.py"):
-            self.load_plugin_from_file(a_plugin_path)
 
         import compat.uniborg
         import compat.paperplane
         import sys
         sys.modules['uniborg'] = compat.uniborg
         sys.modules['userbot'] = compat.paperplane
-        
+
+        for a_plugin_path in Path().glob(f"compat/paperplane/*.py"):
+            self.load_plugin_from_file(a_plugin_path)
+
         for a_plugin_path in Path().glob(f"{self.n_plugin_path}/*.py"):
             self.load_plugin_from_file(a_plugin_path)
 
