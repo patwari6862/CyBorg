d26dc3c67b72879723f345f763ce58b09fc30d86
SnapDragon <maliktahir0702@gmail.com>
2019-09-17 22:14:12 +0200

    Create __init__.py

---
+++ stdplugins/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Init file which loads all of the modules """
+from userbot import LOGS
+from pathlib import Path
+scriptPath = Path(__file__).absolute() # the absolute() is not strictly necessary
+srcPath = scriptPath.parent
+appPath = srcPath.parent
+commonDirectory = appPath.parent # this could have been shortened with scriptPath.parent.parent.parent
+dataPath = commonDirectory / 'dbplugins'
+
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    mod_path.append(glob.glob(datapath + "/*.py))
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
