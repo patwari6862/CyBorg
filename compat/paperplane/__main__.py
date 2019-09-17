0906cfeadab46a366e5b15a21d1b9ffb1ee2102a
SnapDragon <maliktahir0702@gmail.com>
2019-09-17 21:56:40 +0200

    Update __main__.py

--- compat/paperplane/__main__.py
+++ compat/paperplane/__main__.py
@@ -3,6 +3,15 @@ from sys import argv
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import LOGS, bot
 
+INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
+             '\n Tip: Use Country Code along with number.' \
+             '\n or check your phone number and try again !'
+
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
