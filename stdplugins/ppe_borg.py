a825716e471489276f420961da6885118a311af8
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 09:23:59 +0200

    Create ppe_borg.py

---
+++ stdplugins/ppe_borg.py
@@ -0,0 +1,10 @@
+from userbot import bot
+from uniborg.util import admin_cmd
+
+if borg:
+    @borg.on(admin_cmd(pattern="ppe"))
+        async def switch_ppe(event):
+        
+            import userbot.__main__
+            await event.delete
+      cept NameError:
