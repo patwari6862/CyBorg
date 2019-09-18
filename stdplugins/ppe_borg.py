d73552b48a6700468ebffef633153907507874fe
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 12:46:45 +0200

    Update ppe_borg.py

--- stdplugins/ppe_borg.py
+++ stdplugins/ppe_borg.py
@@ -8,10 +8,11 @@ from uniborg.util import admin_cmd
 
 if borg:
     @borg.on(admin_cmd(pattern="ppe"))
-    try:
-        import userbot.__main___
-    except Exception as e:
-        print(str(e))
+    def test():
+        try:
+            import userbot.__main___
+        except Exception as e:
+            print(str(e))
 elif bot:
     @register(outgoing=True, pattern="^.borg")
     async def switch_borg(event):
