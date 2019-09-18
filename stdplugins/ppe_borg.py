cf5169941b8dd7cb23ae0ab99dfb46a3f97f6fbf
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 09:31:51 +0200

    Update ppe_borg.py

--- stdplugins/ppe_borg.py
+++ stdplugins/ppe_borg.py
@@ -1,10 +1,23 @@
-from userbot import bot
+try:
+    from userbot import bot
+    from userbot.events import register
+except:
+    pass
 from uniborg.util import admin_cmd
 
 if borg:
     @borg.on(admin_cmd(pattern="ppe"))
-        async def switch_ppe(event):
-        
+    async def switch_ppe(event):
+        try:
             import userbot.__main__
             await event.delete
-      cept NameError:
+        except NameError:
+            await event.edit("Borg is not loaded")
+elif bot:
+    @register(outgoing=True, pattern=^.borg)
+    async def switch_borg(event):
+        try:
+            import stdborg
+            await event.delete
+        except NameError:
+            await event.edit("Bot is not loaded")
