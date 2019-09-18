7cc554be56b365867d9aa73c4220139d1c862705
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 12:03:38 +0200

    Update __main__.py

--- compat/paperplane/__main__.py
+++ compat/paperplane/__main__.py
@@ -12,22 +12,23 @@ INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
              '\n Tip: Use Country Code along with number.' \
              '\n or check your phone number and try again !'
 
-try:
-    await bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
+async def test()
+    try:
+        bot.start()
+    except PhoneNumberInvalidError:
+        print(INVALID_PH)
+        exit(1)
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("stdplugins." + module_name)
+    for module_name in ALL_MODULES:
+        imported_module = import_module("stdplugins." + module_name)
 
-LOGS.info("Your userbot version is 4.0 - Extended")
+    LOGS.info("Your userbot version is 4.0 - Extended")
 
-LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .alive in any chat."
-    "If you need assistance, head to https://t.me/PaperplaneExtendedChat")
+    LOGS.info(
+        "Congratulations, your userbot is now running !! Test it by typing .alive in any chat."
+        "If you need assistance, head to https://t.me/PaperplaneExtendedChat")
 
-if len(argv) not in (1, 3, 4):
-    await bot.disconnect()
-else:
-    await bot.run_until_disconnected()
+    if len(argv) not in (1, 3, 4):
+        bot.disconnect()
+    else:
+        bot.run_until_disconnected()
