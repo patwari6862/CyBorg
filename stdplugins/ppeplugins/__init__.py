d427341687b92f308729ee4b54e392ebdf3a7568
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 14:06:08 +0200

    Update __init__.py

--- stdplugins/ppeplugins/__init__.py
+++ stdplugins/ppeplugins/__init__.py
@@ -4,6 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 """ Init file which loads all of the modules """
+import sys
 import compat.paperplane
 sys.modules['userbot'] = compat.paperplane
 from userbot import LOGS
